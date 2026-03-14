3924f1f68ad847890aa3583515da21c318300aa6
cmayta <cesarmayta@gmail.com>
2026-03-14 11:32:09 -0500

    ejercicio

--- dia2/flask/primer_app.py
+++ dia2/flask/primer_app.py
@@ -17,5 +17,23 @@ def sumar(a,b):
     resultado =  a + b
     return f"<center>La suma de {a} + {b} es {resultado} </center>"
 
+@app.route('/<operacion>/<int:num1>/<int:num2>')
+def operaciones(operacion,num1, num2):
+    """ dependiendo de la operación mostrar la suma,resta, multiplicación o división """
+    if operacion == 'suma':
+        resultado = num1 + num2
+    elif operacion == 'resta':
+        resultado = num1 - num2
+    elif operacion == 'multiplicacion':
+        resultado = num1 * num2
+    elif operacion == 'division':
+        if num2 == 0:
+            return "<h1>Error: División por cero no permitida</h1>"
+        resultado = num1 / num2
+    else:
+        return "<h1>Error: Operación no válida</h1>"
+
+    return f"<h1>El resultado de la {operacion} entre {num1} y {num2} es {resultado}</h1>"
+
 app.run(debug=True)
 
