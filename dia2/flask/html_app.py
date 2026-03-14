bd53fd45dae3ecaa4f6a0f01e2bf09db15ff89f9
cmayta <cesarmayta@gmail.com>
2026-03-14 12:06:02 -0500

    post

--- dia2/flask/html_app.py
+++ dia2/flask/html_app.py
@@ -7,4 +7,13 @@ def index():
     nombre_request = request.args.get('nombre','')
     return render_template('index.html',nombre=nombre_request)
 
+@app.route('/suma',methods=['GET','POST'])
+def suma():
+    resultado = 0
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        numero2 = request.form['num2']
+        resultado = int(numero1) + int(numero2)
+        return render_template('index.html',resultado=resultado) 
+
 app.run(debug=True)
