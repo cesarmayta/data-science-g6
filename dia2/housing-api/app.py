e9415806c52a60e2f79111a8dafa318f61fa6e43
cmayta <cesarmayta@gmail.com>
2026-03-19 19:39:53 -0500

    housing price

--- dia2/housing-api/app.py
+++ dia2/housing-api/app.py
@@ -1,4 +1,18 @@
 from flask import Flask,request,jsonify
+import joblib
+import numpy as np
+import sklearn
+
+model = joblib.load('./model/model.pkl')
+sc_x = joblib.load('./model/scaler_x.pkl')
+sc_y = joblib.load('./model/scaler_y.pkl')
+
+def predict_price(rooms):
+    rooms_sc = sc_x.transform(np.array([[rooms]]))
+    prediction = model.predict(rooms_sc)
+    prediction_sc = sc_y.inverse_transform(prediction) * 1000
+    price = round(float(prediction_sc[0][0]),2)
+    return price
 
 app = Flask(__name__)
 
@@ -10,5 +24,17 @@ def index():
     }
     return jsonify(context)
 
+@app.route('/housing_price',methods=['POST'])
+def housing_price():
+    rooms = request.json['rooms']
+    price = predict_price(rooms)
+    context = {
+        'message':'precio predicho',
+        'habitaciones': rooms,
+        'precio': price
+    }
+    
+    return jsonify(context)
+    
 if __name__ == '__main__':
     app.run(debug=True)
