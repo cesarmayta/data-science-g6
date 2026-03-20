c65bcf49618f35bf69073c069ed6cf7675ecf449
cmayta <cesarmayta@gmail.com>
2026-03-19 19:57:41 -0500

    sqlalchemy flask

--- dia2/housing-api/app.py
+++ dia2/housing-api/app.py
@@ -1,4 +1,37 @@
 from flask import Flask,request,jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+
+#### CONFIGURACION DE SQLALCHEMY ####
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root2025@localhost:3306/db_g6'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Housing(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    rooms = db.Column(db.Integer,nullable=False)
+    price = db.Column(db.Double,nullable=True)
+    
+    def __init__(self,rooms):
+        self.rooms = rooms
+        
+### CREAMOS UN ESQUEMA PAARA SERIALIZAR LOS DATOS
+ma = Marshmallow(app)
+class HousingSchema(ma.Schema):
+    id = ma.Integer()
+    rooms = ma.Integer()
+    price = ma.Float()
+    
+## REGISTRAMOS LA TABLA EN LA BASE DE DATOS
+db.create_all()
+print('Tablas en base de datos creadas')
+
+
+##### HOUSING ML ################
 import joblib
 import numpy as np
 import sklearn
@@ -14,7 +47,7 @@ def predict_price(rooms):
     price = round(float(prediction_sc[0][0]),2)
     return price
 
-app = Flask(__name__)
+
 
 @app.route('/')
 def index():
