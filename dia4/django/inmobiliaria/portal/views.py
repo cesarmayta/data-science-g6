51e0036df3e207c4507cf2f56cd38c0ad480a3d3
cmayta <cesarmayta@gmail.com>
2026-03-24 20:31:52 -0500

    vistas

--- dia4/django/inmobiliaria/portal/views.py
+++ dia4/django/inmobiliaria/portal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Inmueble
 
 # Create your views here.
+def lista_inmuebles(request):
+    inmuebles = Inmueble.objects.all() # select * from portal_inmueble
+    print(inmuebles)
+    
+    return render(request,'portal/index.html')
