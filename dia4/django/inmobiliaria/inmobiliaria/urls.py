5cf3a4798a168c9f8a1e3599f100a3f1a27b290c
cmayta <cesarmayta@gmail.com>
2026-03-24 20:44:37 -0500

    templates

--- dia4/django/inmobiliaria/inmobiliaria/urls.py
+++ dia4/django/inmobiliaria/inmobiliaria/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from portal.views import lista_inmuebles
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',lista_inmuebles)
+    path('',include('portal.urls'))
 ]
--- dia4/django/inmobiliaria/portal/views.py
+++ dia4/django/inmobiliaria/portal/views.py
@@ -4,6 +4,8 @@ from .models import Inmueble
 # Create your views here.
 def lista_inmuebles(request):
     inmuebles = Inmueble.objects.all() # select * from portal_inmueble
-    print(inmuebles)
-    
-    return render(request,'portal/index.html')
+    #print(inmuebles)
+    context = {
+        'inmuebles':inmuebles
+    }
+    return render(request,'portal/index.html',context)
