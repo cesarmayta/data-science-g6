c4f48366c04ab39eee6a49f1fb62dc13d6c84f46
cmayta <cesarmayta@gmail.com>
2026-03-24 22:28:31 -0500

    migracion mysql

--- dia4/django/inmobiliaria/inmobiliaria/settings.py
+++ dia4/django/inmobiliaria/inmobiliaria/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'inmobiliaria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inmobiliaria',
+        'USER': 'root',
+        'PASSWORD': 'root2025',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
