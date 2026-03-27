989ef4a7258b8ff441f4c1e988588378c83df1cd
cmayta <cesarmayta@gmail.com>
2026-03-26 22:43:59 -0500

    subir paquete

---
+++ dia5/prueba_paquete/ml-test.py
@@ -0,0 +1,10 @@
+#import joblib
+#import numpy as np
+#import sklearn
+
+from housing_predictor import predict_price
+
+
+rooms = int(input("Ingrese el nro de habitaciones : "))
+prediction = predict_price(rooms)
+print(f'El precio de una casa con {rooms} habitaciones es de : $ {prediction}') 
