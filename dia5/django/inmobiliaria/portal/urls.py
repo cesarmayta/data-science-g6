654fd468a60cda271a99ac6dc39cc315f58406b9
cmayta <cesarmayta@gmail.com>
2026-03-26 21:33:51 -0500

    detalle inmueble

--- dia5/django/inmobiliaria/portal/urls.py
+++ dia5/django/inmobiliaria/portal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_inmuebles, name='lista_inmuebles')
+    path('', views.lista_inmuebles, name='lista_inmuebles'),
+    path('inmueble/<int:id>/', views.detalle_inmueble, name='detalle_inmueble'),
 ]
--- dia5/django/inmobiliaria/portal/views.py
+++ dia5/django/inmobiliaria/portal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Inmueble
 
 # Create your views here.
@@ -9,3 +9,11 @@ def lista_inmuebles(request):
         'inmuebles':inmuebles
     }
     return render(request,'portal/index.html',context)
+
+def detalle_inmueble(request,id):
+    
+    inmueble = get_object_or_404(Inmueble,id=id) # select * from portal_inmueble where id = id
+    context = {
+        'inmueble':inmueble
+    }
+    return render(request,'portal/detalle.html',context)
